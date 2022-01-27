fc9d7821901b83a46bef1c8e7b940bdba8d319d3
Matt Oliver <mattoliver.mattoliver@gmail.com>
2022-01-26 20:24:36 -0600

    wip :construction:

--- src/utils/theme-preval.js
+++ src/utils/theme-preval.js
@@ -1,8 +1,8 @@
 // @preval
 // This file needs to be a JavaScript file using CommonJS to be compatiable with preval
-// Cache bust: Jan 02 2022 08:08:16 GMT-0600 (This file is cached by the system's deployment tooling, so you'll need update this timestamp to rebuild this file)
+// Cache bust: Jan 02 2022 08:09:16 GMT-0600 (This file is cached by the system's deployment tooling, so you'll need update this timestamp to rebuild this file)
 
-const { default: primitives } = require("./temp/vars/dist/js");
+const { default: primitives } = require("demo-vars");
 const { partitionColors, fontStack, omitScale } = require("./theme-utils");
 
 console.log("primitives: ", JSON.stringify(primitives));
